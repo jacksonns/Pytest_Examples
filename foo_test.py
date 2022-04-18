42f995f733f4b0fd0806954596ff2101e5a4fbf8
jacksonns <jackson.guima2@gmail.com>
2022-04-18 20:14:26 -0300

    Change foo test

--- foo_test.py
+++ foo_test.py
@@ -1,12 +1,11 @@
-from tkinter import Y
 import pytest
 from src.foo import *
 
 # Wrong answer
-def test_answer1():
+def testanswer1():
     assert inc(3) == 5
 
 # Right answer
-def test_answer2():
+def testanswer2():
     assert inc(4) == 5
 
